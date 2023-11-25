25467525a2ab9a3053c2ee86e36b79f5f99202fa
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-11-25 04:22:54 +0200

    added files to modules

--- common/src/main/java/za/co/mnjonjo/common/facade/TestFacade.java
+++ common/src/main/java/za/co/mnjonjo/common/facade/TestFacade.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import za.co.mnjonjo.core.repository.TestRepository;
-import za.co.mnjonjo.core.utils.SpringUtil;
-
 import java.util.logging.Logger;
 
 /**
@@ -16,8 +14,8 @@ public class TestFacade {
 
     private final static Logger LOGGER = Logger.getLogger(TestFacade.class.getName());
     private TestRepository testRepository;
-    @Autowired
-    @Qualifier("testRepository")
+   /* @Autowired
+    @Qualifier("testRepository")*/
     public void setTestRepository(TestRepository testRepository) {
         this.testRepository =  testRepository;
     }
--- common/src/main/java/za/co/mnjonjo/common/service/TestService.java
+++ common/src/main/java/za/co/mnjonjo/common/service/TestService.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import za.co.mnjonjo.common.facade.TestFacade;
-import za.co.mnjonjo.core.repository.TestRepository;
-import za.co.mnjonjo.core.utils.SpringUtil;
 
 import java.util.logging.Logger;
 
